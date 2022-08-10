4fdda869d3cf1db660f8a2617706cac14c569031
AndreyArtemov <artwise1966@gmail.com>
2022-08-10 08:56:23 +0300

    Полировка сайта

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 from django.core.paginator import Paginator
 def all_blogs(reguest):
-    blogs = Blog.objects.order_by('-date')[:3]
+
+    blogs = Blog.objects.order_by('-date')
 
     return render(reguest, 'blog/all_blogs.html',{'blogs': blogs})
 def detail(reguest,blog_id):
+
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(reguest, 'blog/detail.html', {'blog':blog})
