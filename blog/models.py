f559b9e103c054dbc0102c0b2f61759d6e528ff2
AndreyArtemov <artwise1966@gmail.com>
2022-08-10 07:49:10 +0300

    Создание проекта блог

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.paginator import Paginator
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
 
-# Create your models here.
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+from django.core.paginator import Paginator
+def all_blogs(reguest):
+    blogs = Blog.objects.order_by('-date')[:3]
 
-# Create your views here.
+    return render(reguest, 'blog/all_blogs.html',{'blogs': blogs})
+def detail(reguest,blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(reguest, 'blog/detail.html', {'blog':blog})
